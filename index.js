d5e503ef0baac2e7ac8fcd561e89ac4b7a96f0ca
Slavo Kozar <slavo.kozar@gmail.com>
2019-11-20 22:06:52 +0000

    connection to gpio

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
+const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 const express = require('express')
+
 const app = express()
 const port = 3000
 
 let pins = {
-	0: 0,
-	1: 0
-};
+	0: new Gpio(17, 'out'),
+	1: new Gpio(27, 'out')
+}
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/pin/:id', function (req, res) {
-  res.send(JSON.stringify({status: pins[req.params.id]}));
+	const pin = pins[req.params.id];
+	const status = pin.readSync();
+	res.send(JSON.stringify({status: status}));
 })
 
 app.post('/pin/:id', function (req, res) {
-	pins[req.params.id] = parseInt(req.query.value);
-	res.send(JSON.stringify({status: pins[req.params.id]}));
+	const pin = pins[req.params.id];
+	pin.writeSync(parseInt(req.query.value));
+	const status = pin.readSync();
+	res.send(JSON.stringify({status: status}));
 })
 
 
